d930aed4cc64d9e7132baa76ca3a8cad9f37bd71
EashenMahmud <62823580+EashenMahmud@users.noreply.github.com>
2020-07-29 23:47:08 +0600

    Add files via upload

---
+++ php_task_4/passward.php
@@ -0,0 +1,12 @@
+<fieldset>
+    <legend><b>FORGOT PASSWORD</b></legend>
+
+    <form>
+    	
+		Enter Email:
+        <input type="password" />
+        <hr />
+        <input type="submit" value="Submit" />
+
+    </form>
+</fieldset>
